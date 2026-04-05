69f185d3b1d1d4726cec2a7d06c74828d287bf50
PedroHenriqueRodrigues6 <pedrohmoraes06@gmail.com>
2026-04-05 15:24:37 -0300

    Add files via upload

---
+++ Trabalho01/selection_sort_otimizado.py
@@ -0,0 +1,54 @@
+import random
+import time
+
+def gerador_aleatorio(inicial, final, numeros):
+    random.seed(2026)
+
+    numeros_aleatorios = []
+    for i in range(numeros):
+        numeros_aleatorios.append(random.randint(inicial, final))
+    return numeros_aleatorios
+
+def selection_sort_otimizado(arr):
+    start_time = time.time()
+    inicio = 0
+    fim = len(arr) - 1
+
+    while inicio < fim:
+        menor = inicio
+        maior = inicio
+
+        for i in range(inicio, fim + 1):
+            if arr[i] < arr[menor]:
+                menor = i
+            if arr[i] > arr[maior]:
+                maior = i
+
+        arr[inicio], arr[menor] = arr[menor], arr[inicio]
+
+        if maior == inicio:
+            maior = menor
+
+        arr[fim], arr[maior] = arr[maior], arr[fim]
+
+        inicio += 1
+        fim -= 1
+
+    end_time = time.time()
+    return end_time - start_time
+
+def main():
+    lista_aleatoria = gerador_aleatorio(1, 20000, 10000)
+    lista_ordenada = sorted(lista_aleatoria)
+    lista_inversa = lista_ordenada[::-1]
+
+    tempo_aleatoria = selection_sort_otimizado(lista_aleatoria[:])
+    print("Aleatória:", tempo_aleatoria)
+
+    tempo_ordenada = selection_sort_otimizado(lista_ordenada[:])
+    print("Ordenada:", tempo_ordenada)
+
+    tempo_inversa = selection_sort_otimizado(lista_inversa[:])
+    print("Inversa:", tempo_inversa)
+
+main()
