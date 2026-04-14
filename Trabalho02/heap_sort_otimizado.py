cb8e6889b8fdf2245d712957c62f2628c74fc189
PedroHenriqueRodrigues6 <pedrohmoraes06@gmail.com>
2026-04-14 09:29:46 -0300

    Add files via upload

---
+++ Trabalho02/heap_sort_otimizado.py
@@ -0,0 +1,58 @@
+import random
+import time
+
+def gerador_aleatorio(inicial, final, numeros):
+    random.seed(2026)
+
+    numeros_aleatorios = []
+    for i in range(numeros):
+        numeros_aleatorios.append(random.randint(inicial, final))
+    return numeros_aleatorios
+
+def heapify_iterativo(arr, n, i):
+    while True:
+        maior = i
+        esquerda = 2 * i + 1
+        direita = 2 * i + 2
+
+        if esquerda < n and arr[esquerda] > arr[maior]:
+            maior = esquerda
+
+        if direita < n and arr[direita] > arr[maior]:
+            maior = direita
+
+        if maior == i:
+            break
+
+        arr[i], arr[maior] = arr[maior], arr[i]
+        i = maior
+
+def heap_sort_otimizado(arr):
+    start_time = time.time()
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify_iterativo(arr, n, i)
+
+    for j in range(n - 1, 0, -1):
+        arr[0], arr[j] = arr[j], arr[0]
+        heapify_iterativo(arr, j, 0)
+
+    end_time = time.time()
+    return end_time - start_time
+
+def main():
+    lista_aleatoria = gerador_aleatorio(1, 20000, 10000)
+    lista_ordenada = sorted(lista_aleatoria)
+    lista_inversa = lista_ordenada[::-1]
+
+    tempo_aleatoria = heap_sort_otimizado(lista_aleatoria[:])
+    print("Aleatória:", tempo_aleatoria)
+
+    tempo_ordenada = heap_sort_otimizado(lista_ordenada[:])
+    print("Ordenada:", tempo_ordenada)
+
+    tempo_inversa = heap_sort_otimizado(lista_inversa[:])
+    print("Inversa:", tempo_inversa)
+
+main()
